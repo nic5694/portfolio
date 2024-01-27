480154850d8993104368b42fe5d916ff2ae1d4b6
nic5694 <nicholasmartoccia04@icloud.com>
2024-01-27 14:10:06 -0500

    initial ui project setup

---
+++ ui/tailwind.config.ts
@@ -0,0 +1,50 @@
+// tailwind.config.ts
+import { Config } from 'tailwindcss';
+
+const colors = require('tailwindcss/colors');
+
+const config: Config = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      xsm: '320px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      sky: colors.sky,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      slate: colors.slate,
+      zinc: colors.zinc,
+      neutral: colors.blue,
+      stone: colors.stone,
+      red: colors.red,
+      green: colors.green,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+};
+
+export default config;
