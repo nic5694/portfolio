00c85717e5f4e38366b978d8cd44720a24e93a05
nic5694 <nicholasmartoccia04@icloud.com>
2024-01-27 14:24:53 -0500

    updated project structure

--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -1,50 +1,20 @@
-// tailwind.config.ts
-import { Config } from 'tailwindcss';
-
-const colors = require('tailwindcss/colors');
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    screens: {
-      xsm: '320px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      sky: colors.sky,
-      gray: colors.gray,
-      emerald: colors.emerald,
-      indigo: colors.indigo,
-      yellow: colors.yellow,
-      slate: colors.slate,
-      zinc: colors.zinc,
-      neutral: colors.blue,
-      stone: colors.stone,
-      red: colors.red,
-      green: colors.green,
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
+  plugins: [],
 };
-
 export default config;
