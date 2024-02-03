b4f79359b4b3a4f9058870f650bb2e90f05b160c
nic5694 <nicholasmartoccia04@icloud.com>
2024-02-03 14:36:51 -0500

    removed previous commented out code

--- api/config/db.js
+++ api/config/db.js
@@ -1,21 +1,3 @@
-// const mongoose = require('mongoose');
-// const uri = process.env.DATABASE_URI;
-//
-// const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-//
-// async function run() {
-//     try {
-//         // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-//         await mongoose.connect(uri, clientOptions);
-//         await mongoose.connection.db.admin().command({ ping: 1 });
-//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//     } finally {
-//         // Ensures that the client will close when you finish/error
-//         await mongoose.disconnect();
-//     }
-// }
-// module.exports = run;
-// // run().catch(console.dir);
 const mongoose = require('mongoose');
 const uri = process.env.DATABASE_URI;
 
