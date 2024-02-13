66f33b6d42f7c269307a9cc898eff9952172d1a2
nic5694 <nicholasmartoccia04@icloud.com>
2024-02-13 12:38:29 -0500

    added analytics

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -1,4 +1,5 @@
 import "../globals.css";
+import {Analytics} from "@vercel/analytics/react";
 export default function RootLayout({
                                        children,
                                    }: {
@@ -18,6 +19,7 @@ export default function RootLayout({
         </head>
         <body>
         {children}
+        <Analytics />
         </body>
         </html>
 
