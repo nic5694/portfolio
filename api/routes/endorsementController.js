fc100ba3f237582dc2e4ec8da4ffb6081885bd76
nic5694 <nicholasmartoccia04@icloud.com>
2024-05-13 18:19:37 -0400

    added cors in the endorsement controller

--- api/routes/endorsementController.js
+++ api/routes/endorsementController.js
@@ -3,8 +3,14 @@ var router = express.Router();
 const endorsementService = require('../services/endorsementService')
 // let cors = require('cors');
 var morgan = require('morgan');
+let cors = require('cors');
 
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 //
+}
 router.use(morgan('dev'));
+router.use(cors(corsOptions))
 /* GET home page. */
 router.get('', function(req, res) {
     return endorsementService.getAllEndorsements(res);
